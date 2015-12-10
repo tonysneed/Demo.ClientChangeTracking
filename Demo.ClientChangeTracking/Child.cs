a484fdd8659aa82b881c80fa3461390394e07310
Anthony Sneed <tony@tonysneed.com>
2015-12-10 10:14:39 +0100

    Added Child class that was missing.

---
+++ Demo.ClientChangeTracking/Child.cs
@@ -0,0 +1,18 @@
+﻿using TrackableEntities.Client;
+
+namespace Demo.ClientChangeTracking
+{
+    public class Child : EntityBase
+    {
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
